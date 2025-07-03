9ff0c516ea8e800d10bb579075d186bd012fc57e
Takaaki Gordon Mizuno <takaaki.mizuno@gmail.com>
2025-07-03 17:30:44 +0700

    Image Source Update

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,39 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
   output: 'standalone',
   serverExternalPackages: ['nodemailer'],
+  images: {
+    remotePatterns: [
+      // LocalStack (Development)
+      {
+        protocol: 'http',
+        hostname: 'localstack',
+        port: '4566',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '4566',
+        pathname: '/**',
+      },
+      // AWS S3 (Production)
+      {
+        protocol: 'https',
+        hostname: '*.s3.amazonaws.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.s3.*.amazonaws.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3.amazonaws.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
